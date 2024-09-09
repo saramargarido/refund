aeff261797810dd9237c9f27fcc2f8337c111791
Sara Margarido <sara@blockforce.in>
2024-09-09 15:05:23 +0200

    format value to BRL currency

--- scripts.js
+++ scripts.js
@@ -1,7 +1,22 @@
 // seleciona os elentos do formulário
 const amount = document.getElementById("amount")
 
+// captura o evento de input para formatar o valor
 amount.oninput = () => {
   let value = amount.value.replace(/\D/g, '')
-  amount.value = value
+
+  // transforma o valor em centavos:
+  value =  Number(value) / 100
+
+  amount.value = formatCurrencyBRL(value)
+}
+
+// Formata o valor no padrão BRL
+function formatCurrencyBRL(value) {
+  value = value.toLocaleString("pt-BR", {
+    style: "currency",
+    currency: "BRL"
+  })
+
+  return value
 }
